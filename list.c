fc4331c280ff8df284d2154532f617dfbc5bef57
Replit user <>
2023-08-25 18:01:56 +0000

    new try

--- list.c
+++ list.c
@@ -100,7 +100,7 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * movie, void * data) 
 {
-  movie->current->data  = data;
+  movie->current->next->data  = data;
   movie->tail = NULL;
 }
 
