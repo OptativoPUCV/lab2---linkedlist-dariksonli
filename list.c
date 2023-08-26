049208ff0e67ac24121e1d46221fc27036cc3439
Replit user <>
2023-08-26 02:47:24 +0000

    new try

--- list.c
+++ list.c
@@ -128,7 +128,7 @@ void * popCurrent(List * list)
   if(list == NULL || list -> current == NULL)
       return NULL;
   
-  Node* aux = list->current; 
+  Node* aux = createNode(list->current); 
   if(list->head == list->current)
   {
     list->head = list->current->next;
